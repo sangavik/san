6fe448221c357e9a82775b9ee1e190df00c13218
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-26 16:45:50 +0530

    50power.C

---
+++ 50power.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+printf("\n enter num:");
+scanf("%d",&n);
+if(n/2)
+{
+printf("the gn num is power of 2\n");
+}
+else
+{
+printf("\n not a power of 2");
+}
+return 0;
+}
