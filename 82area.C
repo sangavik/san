caa1adc2140ae34b912df94cf56ac9e2db397346
sangavik <31438501+sangavik@users.noreply.github.com>
2018-02-28 02:18:59 -0800

    82area.C

---
+++ 82area.C
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+float a,b,c;
+printf("\n enter the length:");
+scanf("%d",&a);
+printf("\n enter the breadth:");
+scanf("%d",&b);
+c=a*b;
+printf("the area is %d",c);
+return 0;
+}
