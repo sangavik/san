f82dba8da499e68e2c02745cd044c638d101be37
sangavik <31438501+sangavik@users.noreply.github.com>
2018-03-05 15:56:54 +0530

    98change.C

---
+++ 98change.C
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int i,n,num;
+printf("\n enter the num:");
+scanf("%d",&n);
+printf("\n enter the numbers with one changed number:");
+for(i=1;i=n;i+=)
+{
+scanf("%d",&num[i]);
+}
+for(i=1;i<=n;i++)
+{
+if(i!=num[i])
+{printf("%d",i);
+} else 
+break;
+}
+return 0;
+}
