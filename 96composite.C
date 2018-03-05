ab47fea16aec72cebb4284ce02ec85268f9a3245
sangavik <31438501+sangavik@users.noreply.github.com>
2018-03-05 16:30:15 +0530

    96composite.C

---
+++ 96composite.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i,n,u;
+printf("\n enter the number:");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+if(n%i==0)
+{
+u++;
+}
+}
+if(c==2)
+{
+printf("prime\n");
+}
+else
+printf("composite\n");
+return 0;
+}
