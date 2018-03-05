17eceda40123987e9de021ea02010c64928cff4f
sangavik <31438501+sangavik@users.noreply.github.com>
2018-03-05 16:03:14 +0530

    95interest.C

---
+++ 95interst.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+int si,p,n,r;
+printf("\n enter the values to calculate:");
+scanf("%d%d%d",&p,&n,&r);
+si=(p*n*r)/100;
+printf("\n floor value is :",si);
+return 0;
+}
