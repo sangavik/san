8537b172b5bcb288503fcf2d506ef3c582ea7502
sangavik <31438501+sangavik@users.noreply.github.com>
2018-03-23 16:31:42 +0530

    102divide.C

---
+++ 102divide.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int a,n;
+printf("\n enter the number :");
+scanf("%d",&n);
+if(n%2==0)
+{
+a=n/2;
+printf("\n%d",a);
+}
+else
+{
+printf("\n%d",n);
+}
+return 0;
+}
