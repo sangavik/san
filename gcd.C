cf5ea3877672ee40d7cbf4ae024f88dc8a7fd9e7
sangavik <31438501+sangavik@users.noreply.github.com>
2018-03-26 16:05:20 +0530

    gcd.C

---
+++ gcd.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2,gcd,i;
+printf("\n enter two values:");
+scanf("%d %d",&num1,&num2);
+for(i=1;i<=num1&&i<=num2;i++)
+{
+if((num1%i==0) && (num2%i==0))
+{
+gcd=i;
+}
+}
+printf("\n the gcd of 2 numbers are:%d",gcd);
+return 0;
+}
